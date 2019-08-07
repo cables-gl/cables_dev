efa1ae20d0483d4dd2b1ab67f04970a688426d02
pandrr <tom@drastic.net>
2019-08-07 17:32:22 +0200

    initial stuff

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cables_dev
 cables development environment
+
+
+# visual studio code
+    - install editorconfig extension
---
+++ start.js
@@ -0,0 +1,27 @@
+const concurrently = require('concurrently');
+
+
+
+console.log("STARTY")
+
+
+concurrently([
+    {
+        command:'cd cables_api;npm run start',
+        name:"api",
+        prefixColor:"cyan"
+    },
+    {
+        command:'cd cables_ui;gulp',
+        name:"_ui",
+        prefixColor:"green"
+    }
+], {
+    prefix: 'name',
+    killOthers: ['failure', 'success'],
+    restartTries: 3,
+}).then((success)=>{console.log("success!",success);}).catch(err => console.log('error',err));
+
+
+
+
