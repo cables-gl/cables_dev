435e533fbf7db104cefc864479560250fefbff15
andro <androvisuals@gmail.com>
2019-08-28 10:38:04 +0200

    gitinfo.sh cleaned up code

--- gitinfo.sh
+++ gitinfo.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-currentRepo=""
+#get current branch,accepts string parameter to update current repo 
+testBranch()
+{
+	current="${1}"
+	branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+	echo "CABLES"${current}
+	echo "   current branch is "$branch
+}
+#tests if origin has changed and informs user if they need to pull
+#cannot work correctly with local branches yet
 testOrigin()
 {
 	UPSTREAM=${1:-'@{u}'}
@@ -8,7 +18,6 @@ testOrigin()
 	BASE=$(git merge-base @ "$UPSTREAM")
 	
 	#check if theres no upstream branch here
-	#
 	if [[ $LOCAL = $REMOTE ]]; then
 	    echo "   Up-to-date"
 	elif [[ $LOCAL = $BASE ]]; then
@@ -18,26 +27,7 @@ testOrigin()
 	else
 	    echo "   Diverged"
 	fi
-
 }
-
-testBranch()
-{
-	current="${1}"
-	#git fetch origin
-	#git remote update
-	branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-	#status=$(git status -s)
-	#echo "status of branch is " $status
-
-	echo "CABLES"${current}
-	echo "   branch is "$branch
-	#echo "  status is " $status	
-}
-#branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-#echo $branch
-#git remote -v
-#git branch | grep \* | cut -d ' ' -f2
 
 cd cables
 	testBranch ""
