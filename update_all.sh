a40e876dcf9c3ffffffe65fc4a40f7e761e78b56
Stephan Maihoefer <stephan@undev.de>
2020-04-16 16:05:40 +0200

    eslint and merge develop branches

--- update_all.sh
+++ update_all.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 cd cables
 git pull
+git merge origin/develop
 npm i
 cd ..
 cd cables_api
 npm i
 git pull
+git merge origin/develop
 cd ..
 cd cables_ui
+git pull
+git merge origin/develop
 npm i
 cd ..
