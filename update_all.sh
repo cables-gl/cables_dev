3a31e5fb118c7b76760c578658581ed20dc25403
Stephan Maihoefer <stephan@undev.de>
2020-04-17 10:24:36 +0200

    integrate use of nvm

--- update_all.sh
+++ update_all.sh
@@ -1,16 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+echo "LOADING NODEJS VERSION" `cat .nvmrc`
+. ~/.nvm/nvm.sh
+nvm use
+echo "UPDATING CORE..."
 cd cables
 git pull
 git merge origin/develop
 npm i
 cd ..
+echo "UPDATING API..."
 cd cables_api
 npm i
 git pull
 git merge origin/develop
 cd ..
+echo "UPDATING UI..."
 cd cables_ui
 git pull
 git merge origin/develop
 npm i
 cd ..
+echo "DONE"
