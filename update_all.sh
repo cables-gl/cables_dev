e91ce2b129921f70fa42c9199d5c7532eb6c2c31
Stephan Maihoefer <stephan@undev.de>
2020-04-22 16:11:10 +0200

    make update_all.sh work with local only branches, prevent merging develop into master accidentally

--- update_all.sh
+++ update_all.sh
@@ -6,22 +6,52 @@ set -o pipefail
 echo "LOADING NODEJS VERSION" `cat .nvmrc`
 . ~/.nvm/nvm.sh
 nvm use
+
 echo "UPDATING CORE..."
 cd cables
-git pull
-git merge origin/develop
+# get current branch
+branch=`git rev-parse --abbrev-ref HEAD`
+# ignore errors here, since branch might not be on remote
+git pull origin "$branch" || true
+# merge current remote develop if branch is not master
+if [ "master" != "${branch}" ]; then
+    echo "merging current state of origin/develop into ${branch}";
+    git merge origin/develop;
+else
+    echo "WARNING: not merging origin/develop into master!"
+fi
 npm i
 cd ..
+
 echo "UPDATING API..."
 cd cables_api
+# get current branch
+branch=`git rev-parse --abbrev-ref HEAD`
+# ignore errors here, since branch might not be on remote
+git pull origin "$branch" || true
+# merge current remote develop if branch is not master
+if [ "master" != "${branch}" ]; then
+    echo "merging current state of origin/develop into ${branch}";
+    git merge origin/develop;
+else
+    echo "WARNING: not merging origin/develop into master!"
+fi
 npm i
-git pull
-git merge origin/develop
 cd ..
+
 echo "UPDATING UI..."
 cd cables_ui
-git pull
-git merge origin/develop
+# get current branch
+branch=`git rev-parse --abbrev-ref HEAD`
+# ignore errors here, since branch might not be on remote
+git pull origin "${branch}" || true
+# merge current remote develop if branch is not master
+if [ "master" != "${branch}" ]; then
+    echo "merging current state of origin/develop into ${branch}";
+    git merge origin/develop;
+else
+    echo "WARNING: not merging origin/develop into master!"
+fi
 npm i
 cd ..
 echo "DONE"
