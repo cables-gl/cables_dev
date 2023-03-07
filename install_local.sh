3bf8ecc3c3055ef722606ff43b01126a03bcfe13
Stephan Maihöfer <stephan@undev.de>
2023-03-07 12:51:38 +0100

    fix check cables.json

--- install_local.sh
+++ install_local.sh
@@ -88,6 +88,6 @@ touch cables_ui/scss/svgicons.scss
 mkdir -p cables_api/public/gen/
 touch cables_api/public/gen/opdocs.json
 ./update_all.sh
-if [ ! -f /cables_api/cables.json ]; then
+if [ ! -f cables_api/cables.json ]; then
     cp cables_api/cables_example.json cables_api/cables.json
 fi
