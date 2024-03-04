40dcd54ac734b9425edebf6d876f35cc6e592b5f
Stephan Maihöfer <stephan@undev.de>
2024-03-04 15:51:26 +0100

    no rebuild

--- hook_standalone.sh
+++ hook_standalone.sh
@@ -12,23 +12,17 @@ fi
 echo "building with node version `node --version`"
 
 echo "INSTALLING cables_dev DEPENDENCIES"
-#nvm install
-#nvm use
 npm install
 
 echo "BUILDING shared"
 cd shared
 git pull
-#nvm install
-#nvm use
 npm install
 cd ..
 
 echo "BUILDING cables"
 cd cables
 git pull
-#nvm install
-#nvm use
 npm install
 npm run build
 cd ..
@@ -36,8 +30,6 @@ cd ..
 echo "BUILDING cables_ui"
 cd cables_ui
 git pull
-#nvm install
-#nvm use
 npm install
 npm run build
 cd ..
@@ -45,10 +37,7 @@ cd ..
 echo "BUILDING cables_electron"
 cd cables_electron
 git pull
-#nvm install
-#nvm use
 npm install
-./node_modules/.bin/electron-rebuild
 npm run build
 echo "PACKAGING cables_electron"
 npm run dist$BUILD_OS
