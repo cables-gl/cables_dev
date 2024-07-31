956bd01735c318786c634253172948c98f4a87e1
Stephan Maihöfer <stephan@undev.de>
2024-07-31 18:21:56 +0200

    more info on next steps after install_local.sh

--- install_local.sh
+++ install_local.sh
@@ -198,9 +198,12 @@ npm run build
 cd ..
 
 NPM_START_CMD="'npm run start:standalone'"
+echo -e ""
 if [ "$COMMUNITY_BUILD" = "true" ]; then
   NPM_START_CMD="'npm run start'"
+  echo -e "${YELLOW}BEFORE YOU RUN ${NPM_START_CMD} MAKE SURE YOUR NODE VERSION MATCHES" `cat .nvmrc` "BY RUNNING 'node --version'${NC}"
+else
+  echo -e "${YELLOW}BEFORE YOU RUN ${NPM_START_CMD} MAKE SURE YOUR NODE VERSION MATCHES" `cat .nvmrc` "BY RUNNING 'node --version'${NC}"
+  echo -e "${YELLOW}...then change into cables_electron/ and run 'npm run start' to fire up cables standalone for development${NC}"
 fi
-echo -e ""
-echo -e "${YELLOW}BEFORE YOU RUN ${NPM_START_CMD} MAKE SURE YOUR NODE VERSION MATCHES" `cat .nvmrc` "BY RUNNING 'node --version'${NC}"
 echo -e ""
