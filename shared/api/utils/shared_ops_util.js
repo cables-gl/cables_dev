bcee1364930c19beb823aa1e5f3bedd61fd2a57b
Stephan Maihöfer <stephan@undev.de>
2024-11-05 13:35:46 +0100

    debug opurls

--- shared/api/utils/shared_ops_util.js
+++ shared/api/utils/shared_ops_util.js
@@ -3472,6 +3472,8 @@ export default class SharedOpsUtil extends SharedUtil
             if (envUrl.hostname !== myUrl.hostname) promises.push(fetch(envUrl));
         });
 
+        this._log.debug("OPENV URLS", envUrls);
+
         const envDocs = {
             "id": null,
             "name": null,
