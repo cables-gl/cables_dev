2556bed8f539d51f83f4c0484a9a040b027c5755
Stephan Maihöfer <stephan@undev.de>
2025-01-23 12:07:44 +0100

    platform -> platformLib

--- shared/client/src/ele.js
+++ shared/client/src/ele.js
@@ -5,9 +5,11 @@
  */
 class Ele
 {
+
     /**
      * shortcut for document.getElementById(id)
-     * @param  {String} id
+     *
+     * @param {String} id
      * @returns {Object} DOM element
      */
     byId(id)
@@ -18,7 +20,8 @@ class Ele
 
     /**
      * shortcut for document.querySelector(id)
-     * @param  {String} q
+     *
+     * @param {String} q
      * @returns {Object} DOM element
      */
     byQuery(q)
@@ -28,7 +31,8 @@ class Ele
 
     /**
      * shortcut for document.querySelectorAll(id)
-     * @param  {String} q
+     *
+     * @param {String} q
      * @returns {Array} DOM elements
      */
     byQueryAll(q)
@@ -38,8 +42,9 @@ class Ele
 
     /**
      * returns the first element with class
-     * @param  {String} name
-     * @returns {Object} DOM element
+     *
+     * @param {String} name
+     * @returns {Object|null} DOM element
      */
     byClass(name)
     {
@@ -51,7 +56,8 @@ class Ele
 
     /**
      * returns the all elements with class
-     * @param  {String} name
+     *
+     * @param {String} name
      * @returns {Array} DOM elements
      */
     byClassAll(name)
@@ -62,6 +68,12 @@ class Ele
         return els;
     }
 
+    /**
+     * runs the callback with all elements that have the given class as first argument
+     *
+     * @param {String} name
+     * @param {function(Element)} cb
+     */
     forEachClass(name, cb)
     {
         if (name && name[0] === ".") console.warn("ele.forEachClass should not contain .");
@@ -70,42 +82,60 @@ class Ele
         for (let i = 0; i < eles.length; i++) cb(eles[i]);
     }
 
+    /**
+     * returns the currently selected value for a <select>-element, or the text, if no value is set
+     *
+     * @param {Element} el
+     * @return {*|undefined}
+     */
     getSelectValue(el)
     {
+        if (!el.options || !el.selectedIndex) return;
         return el.options[el.selectedIndex].value || el.options[el.selectedIndex].text;
-    }
-
-    asButton(ele, cb)
-    {
-        this.clickable(ele, cb);
     }
 
     /**
      * makes an element clickable and executes the callback, also add keyboard support, when hitting enter on the element is same as clicking
-     * @param  {Object} element
-     * @returns {Object} DOM element
+     *
+     * @param {Object} element
+     * @param {function(Event)} cb
      */
-    clickable(ele, cb)
+    asButton(element, cb)
     {
-        if (!ele)
-        {
-            // console.log((new Error()).stack);
-            return; // console.log("no ele as button");
-        }
-
-        if (ele.getAttribute("tabindex") == null)ele.setAttribute("tabindex", 0);
-        ele.classList.add("eleAsButton");
-        ele.addEventListener("click", (e) => { cb(e); });
-        ele.addEventListener("keydown", (e) => { if (e.keyCode == 13 || e.keyCode == 32)cb(e); });
+        this.clickable(element, cb);
     }
 
+    /**
+     * makes an element clickable and executes the callback, also add keyboard support, when hitting enter on the element is same as clicking
+     *
+     * @param {Object} element
+     * @param {function(Event)} cb
+     * @returns {Object|undefined} DOM element
+     */
+    clickable(element, cb)
+    {
+        if (!element)
+        {
+            return;
+        }
+
+        if (element.getAttribute("tabindex") == null)element.setAttribute("tabindex", 0);
+        element.classList.add("eleAsButton");
+        element.addEventListener("click", (e) => { cb(e); });
+        element.addEventListener("keydown", (e) => { if (e.keyCode === 13 || e.keyCode === 32)cb(e); });
+    }
+
+    /**
+     * @param {Element} parent
+     * @param {String} query
+     * @param {function(Event, DOMStringMap)} cb
+     */
     clickables(parent, query, cb)
     {
         const clickEles = parent.querySelectorAll(query);
-
         for (let i = 0; i < clickEles.length; i++)
         {
-            ele.clickable(clickEles[i], (e) =>
+            this.clickable(clickEles[i], (e) =>
             {
                 cb(e, e.currentTarget.dataset);
             });
@@ -114,23 +144,34 @@ class Ele
 
     /**
      * can be used for making element keyboard usable and continue using inline onclick e.g. onkepress="ele.keyClick(event,this)"
+     *
+     * @param {Event} event
      * @param  {Object} element
      */
-    keyClick(event, ele)
+    keyClick(event, element)
     {
-        if ((event.keyCode == 13 || event.keyCode == 32) && ele.onclick)ele.onclick();
+        if ((event.keyCode === 13 || event.keyCode === 32) && element.onclick) element.onclick();
     }
 
+    /**
+     * @param {Element} el
+     */
     show(el)
     {
-        if (el)el.classList.remove("hidden");
+        if (el) el.classList.remove("hidden");
     }
 
+    /**
+     * @param {Element} el
+     */
     hide(el)
     {
-        if (el)el.classList.add("hidden");
+        if (el) el.classList.add("hidden");
     }
 
+    /**
+     * @param {Element} el
+     */
     toggle(el)
     {
         if (el.classList.contains("hidden"))
@@ -143,25 +184,38 @@ class Ele
         }
     }
 
+    /**
+     * @param {String} n
+     */
     create(n)
     {
         return document.createElement(n);
     }
 
+    /**
+     * @param {Element} el
+     */
     hasFocus(el)
     {
         return document.activeElement == el;
     }
 
+    /**
+     * @param {Element} el
+     */
     isVisible(el)
     {
         let style = window.getComputedStyle(el);
         return !(style.display === "none");
     }
 
-    append(ele, html)
+    /**
+     * @param {Element} element
+     * @param {String} html
+     */
+    append(element, html)
     {
-        ele.innerHTML += html;
+        element.innerHTML += html;
     }
 }
 
