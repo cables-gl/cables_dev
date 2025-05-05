f8530688fd992525bbc68ff526c11508f72c0915
pandrr <tom@tomkom.net>
2025-05-05 12:03:33 +0200

    events

--- shared/client/src/eventlistener.js
+++ shared/client/src/eventlistener.js
@@ -5,12 +5,14 @@ export class EventListener
      * @param {Object} emitter
      * @param {string} id
      * @param {string} eventName
+     * @param {Function} cb
      */
-    constructor(emitter, id, eventName)
+    constructor(emitter, id, eventName, cb)
     {
         this.targetObj = emitter;
         this.id = id;
         this.eventName = eventName;
+        this.cb = cb;
     }
 
     remove()
--- shared/client/src/eventtarget.js
+++ shared/client/src/eventtarget.js
@@ -7,18 +7,17 @@ import Logger from "./logger.js";
  */
 export default class Events
 {
-    #eventLog;
-    #listeners;
+    #eventLog = new Logger("eventtarget");
+
+    /** @type {Object<string,EventListener>} */
+    #listeners = {};
+    #logEvents = false;
+    #logName = "";
+    #eventCallbacks = {};
+    #countErrorUnknowns = 0;
 
     constructor()
     {
-        this.#eventLog = new Logger("eventtarget");
-        this._eventCallbacks = {};
-        this._logName = "";
-        this._logEvents = false;
-        this.#listeners = {};
-
-        this._countErrorUnknowns = 0;
     }
 
     /**
@@ -26,24 +25,20 @@ export default class Events
      * @param {string} eventName event name
      * @param {function} cb callback
      * @param {string} idPrefix prefix for id, default empty
-     * @return {string} event id
+     * @return {EventListener} eventlistener
      */
     on(eventName, cb, idPrefix = "")
     {
-        // TODO this should be the new eventlistener class...
-        const event =
-            {
-                "id": (idPrefix || "") + helper.simpleId(),
-                "name": eventName,
-                "cb": cb,
-            };
+        const newId = (idPrefix || "") + helper.simpleId();
 
-        if (!this._eventCallbacks[eventName]) this._eventCallbacks[eventName] = [event];
-        else this._eventCallbacks[eventName].push(event);
+        const event = new EventListener(this, newId, eventName, cb);
+
+        if (!this.#eventCallbacks[eventName]) this.#eventCallbacks[eventName] = [event];
+        else this.#eventCallbacks[eventName].push(event);
 
         this.#listeners[event.id] = event;
 
-        return event.id;
+        return event;
     }
 
     /**
@@ -53,19 +48,22 @@ export default class Events
      */
     listen(eventName, cb, idPrefix = "")
     {
-        const id = this.on(eventName, cb, idPrefix);
-        return new EventListener(this, id, eventName);
+        return this.on(eventName, cb, idPrefix);
     }
 
     removeAllEventListeners()
     {
-        for (let i = 0; i < this.#listeners.length; i++)
+        for (const i in this.#listeners)
         {
-            this.off(this.#listeners[i].id);
+            this.off(this.#listeners[i]);
         }
     }
 
-    /** @deprecated */
+    /**
+     *
+     * @param {string} which
+     * @param {Function} cb
+     */
     addEventListener(which, cb, idPrefix = "")
     {
         return this.on(which, cb, idPrefix);
@@ -73,7 +71,7 @@ export default class Events
 
     /**
      * check event listener registration
-     * @param {string} id event id
+     * @param {string|EventListener} id event id
      * @param {function} cb callback - deprecated
      * @return {boolean}
      */
@@ -81,17 +79,20 @@ export default class Events
     {
         if (id && !cb)
         {
-            // check by id
-            return !!this.#listeners[id];
+            if (typeof id == "string") // check by id
+                return !!this.#listeners[id];
+            else
+                return !!this.#listeners[id.id];
+
         }
         else
         {
             this.#eventLog.warn("old eventtarget function haseventlistener!");
             if (id && cb)
             {
-                if (this._eventCallbacks[id])
+                if (this.#eventCallbacks[id])
                 {
-                    const idx = this._eventCallbacks[id].indexOf(cb);
+                    const idx = this.#eventCallbacks[id].indexOf(cb);
                     return idx !== -1;
                 }
             }
@@ -104,9 +105,8 @@ export default class Events
      * @return {boolean}
      */
     hasListenerForEventName(eventName)
-
     {
-        return this._eventCallbacks[eventName] && this._eventCallbacks[eventName].length > 0;
+        return this.#eventCallbacks[eventName] && this.#eventCallbacks[eventName].length > 0;
     }
 
     /** @deprecated */
@@ -117,60 +117,61 @@ export default class Events
 
     /**
      * remove event listener registration
-     * @param {string} id event id
-     * @return
+     * @param {EventListener} listenerParam
      */
-    off(id)
+    off(listenerParam)
     {
-        if (id === null || id === undefined)
+        if (listenerParam === null || listenerParam === undefined)
         {
-            this.#eventLog.warn("removeEventListener id null", id);
+            this.#eventLog.warn("removeEventListener id null", listenerParam);
             return;
         }
 
-        if (typeof id == "string") // new style, remove by id, not by name/callback
-        {
-            const event = this.#listeners[id];
-            if (!event)
-            {
-                if (this._countErrorUnknowns == 20) this.#eventLog.warn("stopped reporting unknown events");
-                if (this._countErrorUnknowns < 20) this.#eventLog.warn("could not find event...", id);
-                this._countErrorUnknowns++;
-                return;
-            }
+        let id = listenerParam;
+        if (listenerParam.eventName) id = listenerParam.id;
 
-            let removeCount = 0;
-
-            let found = true;
-            while (found)
-            {
-                found = false;
-                let index = -1;
-                for (let i = 0; i < this._eventCallbacks[event.name].length; i++)
-                {
-                    if (this._eventCallbacks[event.name][i].id.indexOf(id) === 0) // this._eventCallbacks[event.name][i].id == which ||
-                    {
-                        found = true;
-                        index = i;
-                    }
-                }
-
-                if (index !== -1)
-                {
-                    this._eventCallbacks[event.name].splice(index, 1);
-                    delete this.#listeners[id];
-                    removeCount++;
-                }
-            }
-
-            if (removeCount == 0)console.log("no events removed", event.name, id);
-
-            return;
-        }
-        else
+        if (typeof id != "string")
         {
             console.log("old function signature: removeEventListener! use listener id");
+            return;
         }
+
+        const event = this.#listeners[id];
+        if (!event)
+        {
+            if (this.#countErrorUnknowns == 20) this.#eventLog.warn("stopped reporting unknown events");
+            if (this.#countErrorUnknowns < 20) this.#eventLog.warn("could not find event...", id);
+            this.#countErrorUnknowns++;
+            return;
+        }
+
+        let removeCount = 0;
+
+        let found = true;
+        while (found)
+        {
+            found = false;
+            let index = -1;
+            for (let i = 0; i < this.#eventCallbacks[event.eventName].length; i++)
+            {
+                if (this.#eventCallbacks[event.eventName][i].id.indexOf(id) === 0) // this._eventCallbacks[event.eventName][i].id == which ||
+                {
+                    found = true;
+                    index = i;
+                }
+            }
+
+            if (index !== -1)
+            {
+                this.#eventCallbacks[event.eventName].splice(index, 1);
+                delete this.#listeners[id];
+                removeCount++;
+            }
+        }
+
+        if (removeCount == 0)console.log("no events removed", event.eventName, id);
+
+        return;
     }
 
     /**
@@ -181,8 +182,8 @@ export default class Events
      */
     logEvents(enabled, logName)
     {
-        this._logEvents = enabled;
-        this._logName = logName;
+        this.#logEvents = enabled;
+        this.#logName = logName;
     }
 
     /**
@@ -198,21 +199,21 @@ export default class Events
      */
     emitEvent(which, param1 = null, param2 = null, param3 = null, param4 = null, param5 = null, param6 = null, param7 = null, param8 = null)
     {
-        if (this._logEvents) this.#eventLog.log("[event] ", this._logName, which, this._eventCallbacks);
+        if (this.#logEvents) this.#eventLog.log("[event] ", this.#logName, which, this.#eventCallbacks);
 
-        if (this._eventCallbacks[which])
+        if (this.#eventCallbacks[which])
         {
-            for (let i = 0; i < this._eventCallbacks[which].length; i++)
+            for (let i = 0; i < this.#eventCallbacks[which].length; i++)
             {
-                if (this._eventCallbacks[which][i])
+                if (this.#eventCallbacks[which][i])
                 {
-                    this._eventCallbacks[which][i].cb(param1, param2, param3, param4, param5, param6, param7, param8);
+                    this.#eventCallbacks[which][i].cb(param1, param2, param3, param4, param5, param6, param7, param8);
                 }
             }
         }
         else
         {
-            if (this._logEvents) this.#eventLog.log("[event] has no event callback", which, this._eventCallbacks);
+            if (this.#logEvents) this.#eventLog.log("[event] has no event callback", which, this.#eventCallbacks);
         }
     }
 }
