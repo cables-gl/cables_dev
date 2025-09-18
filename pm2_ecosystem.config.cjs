d6adfc7d1fe7ca31bd7dadb2f7c4b2ee0daf6f1d
Stephan Maihöfer <stephan@undev.de>
2025-09-18 12:51:47 +0200

    test pm2 inspect

--- pm2_ecosystem.config.cjs
+++ pm2_ecosystem.config.cjs
@@ -1,27 +1,44 @@
 module.exports = {
-    "apps": [{
-        "name": "server_api",
-        "script": "src/servers/server_api.js",
-        "cwd": "./cables_api",
-        "instances": 4
-    },
-    {
-        "name": "server_sandbox",
-        "script": "src/servers/server_sandbox.js",
-        "cwd": "./cables_api",
-        "instances": 4
-    },
-    {
-        "name": "server_offload",
-        "script": "src/servers/server_offload.js",
-        "cwd": "./cables_api",
-        "instances": 2
-    },
-    {
-        "name": "server_socketcluster",
-        "script": "src/servers/server_socketcluster.js",
-        "cwd": "./cables_api",
-        "instances": 1,
-        "args": "-w 2 -s 2"
-    }],
+    "apps": [
+        {
+            "name": "server_api",
+            "script": "src/servers/server_api.js",
+            "cwd": "./cables_api",
+            "interpreter_args": "--inspect=127.0.0.1:9230",
+            "exec_mode": "cluster"
+        },
+        {
+            "name": "server_api",
+            "script": "src/servers/server_api.js",
+            "cwd": "./cables_api",
+            "interpreter_args": "--inspect=127.0.0.1:9231",
+            "exec_mode": "cluster"
+        },
+        {
+            "name": "server_api",
+            "script": "src/servers/server_api.js",
+            "cwd": "./cables_api",
+            "interpreter_args": "--inspect=127.0.0.1:9232",
+            "exec_mode": "cluster"
+        },
+        {
+            "name": "server_api",
+            "script": "src/servers/server_api.js",
+            "cwd": "./cables_api",
+            "interpreter_args": "--inspect=127.0.0.1:9233",
+            "exec_mode": "cluster"
+        },
+        {
+            "name": "server_sandbox",
+            "script": "src/servers/server_sandbox.js",
+            "cwd": "./cables_api",
+            "instances": 4
+        },
+        {
+            "name": "server_socketcluster",
+            "script": "src/servers/server_socketcluster.js",
+            "cwd": "./cables_api",
+            "instances": 1,
+            "args": "-w 2 -s 2"
+        }]
 };
