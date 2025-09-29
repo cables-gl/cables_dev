645997c29da67393b564a0a987ee9e5ec0e02e42
Stephan Maihöfer <stephan@undev.de>
2025-09-29 16:26:43 +0200

    instanceid

--- pm2_ecosystem.config.cjs
+++ pm2_ecosystem.config.cjs
@@ -4,41 +4,22 @@ module.exports = {
             "name": "server_api",
             "script": "src/servers/server_api.js",
             "cwd": "./cables_api",
-            "interpreter_args": "--inspect=127.0.0.1:9230",
-            "exec_mode": "cluster"
-        },
-        {
-            "name": "server_api",
-            "script": "src/servers/server_api.js",
-            "cwd": "./cables_api",
-            "interpreter_args": "--inspect=127.0.0.1:9231",
-            "exec_mode": "cluster"
-        },
-        {
-            "name": "server_api",
-            "script": "src/servers/server_api.js",
-            "cwd": "./cables_api",
-            "interpreter_args": "--inspect=127.0.0.1:9232",
-            "exec_mode": "cluster"
-        },
-        {
-            "name": "server_api",
-            "script": "src/servers/server_api.js",
-            "cwd": "./cables_api",
-            "interpreter_args": "--inspect=127.0.0.1:9233",
-            "exec_mode": "cluster"
+            "instances": 4,
+            "instance_var": "PM2_INSTANCE_ID"
         },
         {
             "name": "server_sandbox",
             "script": "src/servers/server_sandbox.js",
             "cwd": "./cables_api",
-            "instances": 4
+            "instances": 4,
+            "instance_var": "PM2_INSTANCE_ID"
         },
         {
             "name": "server_socketcluster",
             "script": "src/servers/server_socketcluster.js",
             "cwd": "./cables_api",
             "instances": 1,
-            "args": "-w 2 -s 2"
+            "args": "-w 2 -s 2",
+            "instance_var": "PM2_INSTANCE_ID"
         }]
 };
