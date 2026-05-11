3657b700c29baa8fa3c5b9811b6164735fce577c
steam0r <stephan@undev.de>
2026-05-11 09:37:09 +0200

    mark markdown - cables-gl/cables/issues/8292

--- shared/client/src/handlebars.js
+++ shared/client/src/handlebars.js
@@ -18,7 +18,7 @@ class HandlebarsHelper
                 let escaped = Handlebars.escapeExpression(str);
                 if (marked) escaped = marked.parse(escaped);
                 if (setOpLinks) escaped = this._setOpLinks(escaped, linkTarget);
-                return new Handlebars.SafeString(escaped);
+                return new Handlebars.SafeString("<div class=\"md\">" + escaped + "</div>");
             });
 
             Handlebars.registerHelper("round", (str) =>
