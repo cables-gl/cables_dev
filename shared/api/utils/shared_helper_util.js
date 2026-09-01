27c4e1f82498d08fe9c7d20c076eaae498c6c286
steam0r <stephan@undev.de>
2026-09-01 09:25:46 +0200

    sanitize username

--- shared/api/utils/shared_helper_util.js
+++ shared/api/utils/shared_helper_util.js
@@ -117,7 +117,9 @@ export default class SharedHelperUtil extends SharedUtil
         name = name.toLowerCase();
         name = name.split(" ").join("_");
         name = name.replace(/\./g, "_");
-        if (name.match(/^\d/))name = "u_" + name;
+        if (name.match(/^\d/)) name = "u_" + name;
+        // TODO: this should be a list of reserved words - cables-gl/cables/issues/8530
+        if (name === "constructor") name = "u_" + name;
         return name;
     }
 
