1d4f041ffba97c80ad4480acba44d179e40146d8
pandrr <tom@undev.de>
2026-09-15 01:29:27 -0500

    ele id...

--- shared/client/src/ele.js
+++ shared/client/src/ele.js
@@ -142,8 +142,8 @@ class Ele
 
     /**
      * @callback eleClickableCallback
-     * @param {Event}
-     * @param {Dataset}
+     * @param {Event} event
+     * @param {DOMStringMap} dataset
      */
 
     /**
@@ -160,7 +160,7 @@ class Ele
         {
             this.clickable(clickEles[i], (e) =>
             {
-                cb(e, e.currentTarget.dataset);
+                cb(e, /** @type {HTMLElement} */ (e.currentTarget).dataset);
             });
         }
     }
